0db90a29544b69693d7dc58b0868f37c0616f1d9
Zaki abdullahi <qable2121@gmail.com>
2023-08-29 23:40:51 +0300

    added fixed cors7

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: 'https://blog-api-vhtu.onrender.com/'
 
-  },
   plugins: [react()],
 })
